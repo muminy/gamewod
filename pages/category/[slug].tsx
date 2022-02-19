f00a0b773438e1cb219cb040ddd47ca4fd181529
Mümin Yıldırım <mmnyldrm61@gmail.com>
2022-02-19 09:34:46 +0300

    Update [slug].tsx

--- pages/category/[slug].tsx
+++ pages/category/[slug].tsx
@@ -13,6 +13,8 @@ import { menus } from "constants/datas";
 import { ArticleProps, ICategory, MenuCategoryProps } from "constants/types";
 import { category_posts } from "services/article/config";
 import { ApiInstance } from "services/apis";
+import { useRouter } from "next/router";
+import { ArticleSkeleton } from "components/Skeleton/Article";
 
 interface IProps {
   category: MenuCategoryProps;
@@ -21,6 +23,7 @@ interface IProps {
 
 export default function Category(props: IProps) {
   const { category, posts } = props;
+  const router = useRouter();
 
   const title = `${category?.title} | Gamewod.com`;
   const description = `${category?.title} Alanındaki son gelişmeler`;
@@ -29,30 +32,32 @@ export default function Category(props: IProps) {
     <Layout seo={{ title, description }}>
       <CategoryHeader {...category} />
 
-      {posts.length === 0 && (
-        <div className={STYLE.paddingHorizontal}>
-          <NoData title="Bu Kategoriye Ait Paylaşım Bulunamadı" />
-        </div>
-      )}
-
       <Grid.Col
         cols="2xl:grid-cols-5 xl:grid-cols-6 grid-cols-6"
         className={classNames(STYLE.paddingHorizontal, "xl:gap-x-10")}
       >
-        {posts.map((item: ICategory) =>
-          item.attributes.posts.data.length === 0 ? (
-            <Grid.Span span={"col-span-12"}>
-              <NoData title="Bu Kategoriye Ait Paylaşım Bulunamadı" />
-            </Grid.Span>
-          ) : (
-            item.attributes.posts.data.map((item: ArticleProps) => (
-              <Grid.Span
-                span="2xl:col-span-1 xl:col-span-2 lg:col-span-3 md:col-span-6 col-span-12"
-                key={item.id}
-              >
-                <BlogCard {...item} />
+        {router.isFallback ? (
+          <ArticleSkeleton />
+        ) : posts.length === 0 ? (
+          <Grid.Span span={"col-span-12"}>
+            <NoData title="Bu Kategoriye Ait Paylaşım Bulunamadı" />
+          </Grid.Span>
+        ) : (
+          posts.map((item: ICategory) =>
+            item.attributes.posts.data.length === 0 ? (
+              <Grid.Span span={"col-span-12"}>
+                <NoData title="Bu Kategoriye Ait Paylaşım Bulunamadı" />
               </Grid.Span>
-            ))
+            ) : (
+              item.attributes.posts.data.map((item: ArticleProps) => (
+                <Grid.Span
+                  span="2xl:col-span-1 xl:col-span-2 lg:col-span-3 md:col-span-6 col-span-12"
+                  key={item.id}
+                >
+                  <BlogCard {...item} />
+                </Grid.Span>
+              ))
+            )
           )
         )}
       </Grid.Col>
@@ -79,7 +84,7 @@ export async function getStaticProps(context: GetStaticPropsContext) {
 
 export async function getStaticPaths() {
   return {
-    paths: menus.map((item) => `/category/${item.id}`),
+    paths: menus.map((item) => ({ params: { slug: item.id } })),
     fallback: true,
   };
 }
